c89fe64e2060a974a2613e600335d4eb6d2491a0
huangzh <huangzh@himeidian.com>
2019-09-04 15:08:44 +0800

    build脚本修改

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 #! /bin/bash
 
-cd ~/hx/hx
-
-echo ">>>>>>>您当前位于分支:<<<<<<<"
-git status
+cd ~/hx
 
 echo ">>>>>>>开始更新代码<<<<<<<"
 git pull
@@ -12,13 +9,8 @@ echo ">>>>>>>开始打jar包:<<<<<<<"
 gradle clean build -x test
 echo "<<<<<<<jar包已build完成>>>>>>>"
 
-echo ">>>>>>>进入到jar包目录<<<<<<<"
-cd ~/hx/hx/build/libs
-
-echo "<<<<<<<开始杀死原进程>>>>>>>"
+echo "<<<<<<<开始杀死原进程并启动Java程序>>>>>>>"
+cd ~/hx/build/libs
 pkill -9 java
-echo "<<<<<<<java进程已杀死>>>>>>>"
-
-echo "<<<<<<<开始启动Java程序>>>>>>>"
 nohup java -jar hx-0.0.1-SNAPSHOT.jar &
-echo "<<<<<<<启动完成>>>>>>>"
+echo "<<<<<<<执行完成，Java正在启动中，20秒后启动完成>>>>>>>"
